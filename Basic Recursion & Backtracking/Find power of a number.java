02765dd794f3f7068ab1166da1354418ba71a3e5
Ameya kawade <114289165+Ameya-kawade@users.noreply.github.com>
2023-07-02 22:02:03 +0530

    Update Find power of a number.java

--- Basic Recursion & Backtracking/Find power of a number.java
+++ Basic Recursion & Backtracking/Find power of a number.java
@@ -4,13 +4,15 @@ import java.io.*;
 class Solution {
 
 	public static long Pow(int X, int N) {
-		//Write your code here
+	//Write your code here
+		if(X == 0){
+	            return 0;
+	        }
+		
 		if(N == 0){
-			return 1;
+		return 1;
 		}
-        if(X == 0){
-            return 0;
-        }
+	        
 		return X*Pow(X,N-1);
 	}
 }
